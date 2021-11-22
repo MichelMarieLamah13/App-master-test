abf4128fec0f763e36b2556a5b98ac64d5536d85
Michel Marie LAMAH <lamahmichelmarie@gmail.com>
2021-11-22 17:48:19 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,651 @@
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { AgmInfoWindow } from '@agm/core';
+import { DataModel, DeviceList, EventData, marker } from 'src/models/Response';
+import { MyServiceService } from './my-service.service';
+import { DeviceList1, EventData1, Response1, tableModel } from 'src/models/Response2';
+import { table } from 'console';
+import { ExpandEventArgs } from '@syncfusion/ej2-angular-navigations';
+import { interval } from 'rxjs';
+import { User } from 'src/models/User';
+import { ResizeEvent } from 'angular-resizable-element';
+import { viewParentEl } from '@angular/core/src/view/util';
+import { EChartOption } from 'echarts';
+import { MouseEvent, AgmMap } from '@agm/core';
+import { TravelMarker, TravelMarkerOptions, TravelData, TravelEvents, EventType } from 'travel-marker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string = 'Map App';
+  lat: number = 22.0574;
+  lng: number = 78.9382;
+  zoom: number = 4;
+  marker: marker[];
+  url: string;
+  url2: string;
+  loader: boolean = false;
+  singleMarker: marker = new marker();
+  DeviceList: DeviceList[];
+  EventData: EventData[];
+  SingleEventData: EventData = new EventData();
+
+  DataModel: DataModel;
+  Response1: Response1 = new Response1();
+  DeviceList1: DeviceList1[];
+  EventData1: EventData1[];
+  tableModelList = new Array<tableModel>();
+  tableModel: tableModel;
+  MarkerTitleArray: String[];
+  @Input() isLoader: Boolean;
+  isExpanded: boolean;
+  ExistDeviceID: string;
+  CountLength: number = 0;
+  icon = {
+    labelOrigin: { x: 16, y: 48 },
+    url: "./assets/images/Voiture_ 2.svg",
+    scaledSize: {
+      width: 20,
+      height: 40
+    }
+  };
+
+  icon1 = {
+    labelOrigin: { x: 16, y: 48 },
+    url: "./assets/images/vehicle.png",
+    scaledSize: {
+      width: 20,
+      height: 40
+    },
+    rotation: 90
+  };
+  icon2 = {
+    labelOrigin: { x: 16, y: 48 },
+    url: "./assets/images/voitureEnMarche.png",
+    scaledSize: {
+      width: 20,
+      height: 40
+    },
+    rotation: 190
+  };
+
+
+  iconSymbol1 = {
+    path: "./assets/images/vehicle.svg",
+    rotation: 0,
+    anchor: { x: 16, y: 48 }
+  }
+  iconSymbol2 = {
+    path: "./assets/images/Voiture_ 2.svg",
+    rotation: 0,
+    anchor: { x: 16, y: 48 }
+  }
+
+
+  clusterOptions = {
+    imagePath: "https://raw.githubusercontent.com/googlemaps/v3-utility-library/master/markerclustererplus/images/m",
+    gridSize: 30,
+    zoomOnClick: false,
+    maxZoom: 10,
+    minimumClusterSize: 2
+  };
+
+  mapOptions = {
+    fitbounds: true,
+    latitude: 0,
+    longitude: 0,
+    maxZoom: null,
+    minZoom: 3,
+    zoom:8,
+    disableDefaultUI: true,
+    styles: [{ featureType: "administrative.country", stylers: [{ visibility: "off" }] }]
+  }
+
+  user: User = new User();
+  localhost: string = 'backup.sendatrack.com';
+
+
+  isStart = false;
+  isFinal = false;
+  start = new EventData1();
+  final = new EventData1();
+
+  @ViewChild('container') container: ElementRef;
+  @ViewChild('leftPanel') leftPanel: ElementRef;
+  @ViewChild('rightPanel') rightPanel: ElementRef;
+  @ViewChild('ngbTabset') ngbTabset: any;
+
+  maxWidthLeftRight: number = 0;
+  maxHeightLeftRight: number = 0;
+  maxHeightTopBottom: number = 0;
+  demoChartInit: number = 60
+  demoChartHeight: number;
+  constructor(private service: MyServiceService) {
+
+  }
+  ngOnInit() {
+    this.GetConnectedUser();
+    this.GetResponse();
+    this.setMapCenter();
+    this.maxWidthLeftRight = this.leftPanel.nativeElement.offsetWidth;
+    this.maxHeightTopBottom = this.rightPanel.nativeElement.offsetHeight;
+    this.maxHeightLeftRight = this.container.nativeElement.offsetHeight;
+    this.leftStyle = {
+      ...this.leftStyle,
+      'height': `${this.maxHeightLeftRight + 100}px`
+    }
+    this.demoChartHeight = this.demoChartInit;
+  }
+
+  defaultUser: User[] = [
+    new User("motivation", "admin", "motivation123321"),
+    new User("alaqsa", "admin", "19641964"),
+    new User("actitrans","admin","666666")
+  ];
+  GetConnectedUser() {
+    let compte = localStorage.getItem('compte');
+    let login = localStorage.getItem('login');
+    let motDePass = localStorage.getItem('motDePass');
+    let server = localStorage.getItem('serveur');
+    this.user.compte = compte ? compte : this.defaultUser[2].compte;
+    this.user.login = login ? login : this.defaultUser[2].login;
+    this.user.motDePass = motDePass ? motDePass : this.defaultUser[2].motDePass;
+    this.localhost = server ? server : this.localhost;
+  }
+
+
+  //in 10 seconds do something
+  GetResponse() {
+    this.url = 'http://' + this.localhost + ':8080/events7/data.jsonx?a=' + this.user.compte + '&p=' + this.user.motDePass + '&u=' + this.user.login + '&g=all&l=1&at=true';
+    this.service.GetData1(this.url).subscribe(
+      data => {
+        let da = data as any;
+        localStorage.setItem("DeviceList", JSON.stringify(da.DeviceList));
+        let LocalDeviceList = JSON.parse(localStorage.getItem("DeviceList"));
+        if (LocalDeviceList == null || (LocalDeviceList.length != this.CountLength)) {
+          console.log("5 sece")
+          this.CountLength = LocalDeviceList.length;
+          this.DataModel = da;
+          this.EventData = [];
+          this.DeviceList = this.DataModel.DeviceList;
+          // For filtering
+          this.DeviceListFiltered = this.DeviceList;
+          this.DeviceList.forEach(deviceData => {
+            deviceData.EventData.forEach(eventDataItem => {
+              eventDataItem.markerDescription = deviceData.Device_desc;
+              eventDataItem.isActive = true;
+              eventDataItem.icon = this.getIcon(eventDataItem);
+              this.EventData.push(eventDataItem);
+            });
+          });
+          localStorage.setItem("EventDataVehicle", JSON.stringify(this.EventData));
+        }
+        if (this.CountLength == LocalDeviceList.length) {
+          console.log(" 5 seconds equal");
+        }
+
+        //
+
+      },
+      err => console.error(err),
+      () => console.log('5  done loading')
+    );
+    setTimeout(() => {
+      this.GetResponse();
+      //window.location.reload();
+    }, 5000);
+  }
+  previousWindow: AgmInfoWindow = null;
+
+  clickMarker(infoWindow: AgmInfoWindow) {
+    if (this.previousWindow) {
+
+      this.previousWindow.close();
+
+    }
+    this.previousWindow = infoWindow;
+  }
+  //   fieldsChange(values:any,id:any) {
+  //    this.GetTableDatAndFill(values,id);
+  //  }
+
+  GetTableDatAndFill(checkBoxValue: any, DeviceId: any) {
+
+    //  this.isLoader=true;
+    //remove from map
+
+    var chxBox = checkBoxValue.currentTarget.checked;
+    if (!chxBox) {
+      this.EventData = this.EventData.filter(item => item.Device !== DeviceId);
+    } else {
+      let data = JSON.parse(localStorage.getItem("EventDataVehicle"));
+
+      this.SingleEventData = data.find(item => item.Device === DeviceId);
+      this.EventData.push(this.SingleEventData);
+    }
+
+
+
+
+  }
+
+
+  tempPath: any[] = [];
+  expanded(DeviceId: string, DeviceDesc: string = "") {
+    this.latLngAllPath = [];
+    this.tempPath = [];
+    console.log(DeviceId);
+    this.showMap = true;
+    this.isLoader = true;
+    if (this.ExistDeviceID != DeviceId) {
+      this.ngbTabset.select("1");
+      this.url = 'http://' + this.localhost + ':8080/eventsApp2/data.jsonx?a=' + this.user.compte + '&p=' + this.user.motDePass + '&u=' + this.user.login + '&d=' + DeviceId + '&rf=1625443200&rt=1625529599&l=5000&at=true';
+      //http://backup.sendatrack.com:8080/events7/data.jsonx?a=motivation&p=motivation123321&u=admin&d=v11&rf=1625443200&rt=1625529599&l=5000&at=true
+      //http://gsAddressServApp:8080/events7/data.jsonx?a=motivation&p=motivation123321&u=admin&d=v11&rf=1625443200&rt=1625529599&l=5000&at=true
+      this.service.GetData1(this.url).subscribe(
+        data => {
+          let da = data as any;
+          this.Response1 = da;
+          //fill table
+          this.tableModelList = [];
+          this.chartData = [];
+          this.DeviceList1 = this.Response1.DeviceList;
+          this.DeviceList1.forEach(deviceData1 => {
+            if (deviceData1.EventData.length) {
+              deviceData1.EventData.forEach((eventDataItem, i) => {
+                this.chartData.push([new Date(eventDataItem.Timestamp * 1000), eventDataItem.Speed])
+                this.tableModel = new tableModel();
+                if (eventDataItem.StatusCode == 62465 || eventDataItem.StatusCode == 61714) {
+                  if (!this.isStart) {
+                    this.start = eventDataItem;
+                    this.isStart = true;
+                  } else if (i == deviceData1.EventData.length - 1) {
+                    this.final = eventDataItem;
+                    this.isFinal = true;
+                  }
+
+                } else if (eventDataItem.StatusCode == 62467) {
+                  if (this.isStart && !this.isFinal) {
+                    this.final = eventDataItem;
+                    this.isFinal = true;
+                  }
+                }
+
+                if (this.isStart) {
+                  this.tempPath.push(eventDataItem);
+                }
+
+                if (this.isStart && this.isFinal) {
+                  this.tableModel.start = this.start.Timestamp * 1000;
+                  this.tableModel.initialPosition = this.start.Address;
+                  this.tableModel.final = this.final.Timestamp * 1000;
+                  this.tableModel.finalPosition = this.final.Address;
+                  this.tableModel.duration = this.convertHMS(this.final.Timestamp - this.start.Timestamp);
+                  this.tableModel.distance = this.final.Odometer - this.start.Odometer;
+
+                  this.tableModelList.push(this.tableModel);
+                  this.isLoader = false;
+                  this.ExistDeviceID = DeviceId;
+
+                  this.isStart = false;
+                  this.isFinal = false;
+                  this.latLngAllPath.push(this.tempPath);
+                  this.tempPath = [];
+                }
+              });
+            } else {
+              this.isLoader = false;
+              this.ExistDeviceID = DeviceId;
+
+              this.isStart = false;
+              this.isFinal = false;
+            }
+
+          });
+          let x: number = this.tableModelList.length
+          if (x) {
+            this.setTopBottomHeightOnClick(x);
+            this.getEchartOptions(DeviceDesc);
+          }
+        },
+        err => console.error(err),
+        () => console.log('done loading ')
+      );
+
+    }
+    else if (this.ExistDeviceID == DeviceId) {
+      this.clearFromTitle();
+    }
+
+
+  }
+
+  clearFromTitle(mar:EventData=null) {
+    this.tableModelList = [];
+    this.isLoader = false;
+    this.showMap = true;
+    this.ExistDeviceID = "";
+    this.setTopBottomHeightOnClick(0);
+    this.chartOption = {};
+    if(mar){
+      this.mapOptions.latitude=mar.GPSPoint_lat;
+      this.mapOptions.longitude=mar.GPSPoint_lon;
+      this.mapOptions.zoom=22;
+    }
+  }
+  setTopBottomHeightOnClick(x: number) {
+    this.containerHeight = this.rightPanel.nativeElement.offsetHeight;
+    //this.bottomHeight = x ? 160 + 80 * (x - 1) : 160;
+    this.bottomHeight = x ? 160 + 300 : 160;
+    this.topHeight = this.containerHeight - this.bottomHeight
+    this.setGridHeightTop();
+    this.demoChartHeight = x ? 400 : this.demoChartInit;
+  }
+  setMapCenter() {
+    var geo = navigator.geolocation;
+    if (geo) {
+      geo.getCurrentPosition(
+        (position) => {
+          this.mapOptions.latitude = position.coords.latitude;
+          this.mapOptions.longitude = position.coords.longitude;
+        },
+        (error) => {
+          console.log(error)
+        }
+      )
+    }
+  }
+
+  convertHMS(value: number) {
+    let hours = Math.floor(value / 3600); // get hours
+    let minutes = Math.floor((value - (hours * 3600)) / 60); // get minutes
+    let seconds = value - (hours * 3600) - (minutes * 60); //  get seconds
+    // add 0 if value < 10; Example: 2 => 02
+    let sHours = (hours < 10) ? '0' + hours : hours;
+    let sMinutes = (minutes < 10) ? '0' + minutes : minutes;
+    let sSeconds = (seconds < 10) ? '0' + seconds : seconds;
+    let res = '';
+    if (hours) {
+      res += sHours + 'h';
+    }
+    if (minutes) {
+      res += sMinutes + 'min';
+    }
+    if (seconds) {
+      res += sSeconds + 's';
+    }
+    return res;
+  }
+
+  rightStyle: object = {
+  };
+  leftStyle: object = {
+  };
+  topStyle: object = {
+  };
+  bottomStyle: object = {
+  };
+  containerWidth: number = 0;
+  containerHeight: number = 0;
+  leftWidth: number = 0;
+  rightWidth: number = 0;
+  topHeight: number = 0;
+  bottomHeight: number = 0;
+  setGridWidthLeft() {
+    this.leftStyle = {
+      ...this.leftStyle,
+      'max-width': `${this.leftWidth}px`,
+    };
+    this.rightStyle = {
+      ...this.rightStyle,
+      'min-width': `${this.rightWidth}px`,
+    }
+  }
+  setGridHeightTop() {
+    this.topStyle = {
+      ...this.topStyle,
+      'max-height': `${this.topHeight}px`,
+    };
+    this.bottomStyle = {
+      ...this.bottomStyle,
+      'min-height': `${this.bottomHeight}px`,
+    }
+
+  }
+  onResizingLeft(event: ResizeEvent): void {
+    this.containerWidth = this.container.nativeElement.offsetWidth;
+    this.leftWidth = event.rectangle.width;
+    this.rightWidth = this.containerWidth - this.leftWidth;
+    this.setGridWidthLeft();
+  }
+  validateLeft(event: ResizeEvent): boolean {
+    const MIN_DIMENSIONS_PX: number = 100;
+    const MAX_DIMENSIONS_PX: number = this.maxWidthLeftRight;
+    if (event.rectangle.width < MIN_DIMENSIONS_PX || event.rectangle.width > MAX_DIMENSIONS_PX) {
+      return false;
+    }
+    return true;
+  }
+
+  onResizingTop(event: ResizeEvent): void {
+    this.containerHeight = this.rightPanel.nativeElement.offsetHeight;
+    this.topHeight = event.rectangle.height;
+    this.bottomHeight = this.containerHeight - this.topHeight;
+    this.setGridHeightTop();
+  }
+  validateTop(event: ResizeEvent): boolean {
+    const MIN_DIMENSIONS_PX: number = 100;
+    const MAX_DIMENSIONS_PX: number = this.maxHeightTopBottom;
+    if (event.rectangle.height < MIN_DIMENSIONS_PX || event.rectangle.height > MAX_DIMENSIONS_PX) {
+      return false;
+    }
+    return true;
+  }
+
+
+  getIcon(marker: EventData) {
+    var icon = {
+      labelOrigin: { x: 40, y: 85 }, //16,48
+      url: "./assets/images/va.png",
+      scaledSize: {
+        width: 90,//20
+        height: 90//40
+      }
+    };
+    if (marker.StatusCode != 62467) {
+      switch (marker.Heading_desc) {
+        case "N":
+          icon.url = "./assets/images/vmn.png";
+          break;
+        case "NE":
+          icon.url = "./assets/images/vmne.png";
+          break;
+        case "NO":
+          icon.url = "./assets/images/vmno.png";
+          break;
+        case "S":
+          icon.url = "./assets/images/vms.png";
+          break;
+        case "SE":
+          icon.url = "./assets/images/vmse.png";
+          break;
+        case "SO":
+          icon.url = "./assets/images/vmso.png";
+          break;
+
+        case "E":
+          icon.url = "./assets/images/vme.png";
+          break;
+        case "O":
+          icon.url = "./assets/images/vmo.png";
+          break;
+
+        default:
+          icon.url = "./assets/images/vmn.png";
+          break;
+      }
+    }
+    return icon;
+  }
+
+  public valueToSearch: string = "";
+  public DeviceListFiltered: DeviceList[] = [];
+  filterAccordion($event) {
+    this.DeviceListFiltered = this.DeviceList.filter((value) => {
+      var x = value.Device_desc.includes(this.valueToSearch);
+      if (x) {
+        return value;
+      }
+    });
+  }
+
+  chartOption: EChartOption = {
+  }
+  chartData: Array<Array<any>> = []
+  getEchartOptions(DeviceDesc: string = "") {
+    this.chartOption = {
+      tooltip: {
+        trigger: 'axis',
+        position: function (pt) {
+          return [pt[0], '10%'];
+        }
+      },
+      title: {
+        left: 'center',
+        text: 'Vitesse à chaque instant de ' + DeviceDesc
+      },
+      toolbox: {
+        feature: {
+          dataZoom: {
+            yAxisIndex: 'none',
+            title: {
+              zoom: 'Zoom sur zone',
+              back: 'Enlever le zoom'
+            }
+          },
+          restore: { title: 'Restaurer' },
+          saveAsImage: { title: 'Télécharger' }
+        }
+      },
+      xAxis: {
+        name: 'Date et Heure',
+        type: 'time',
+        boundaryGap: false
+      },
+      yAxis: {
+        name: 'Vitesse[Km/h]',
+        type: 'value',
+        boundaryGap: false
+      },
+      dataZoom: [
+        {
+          type: 'inside',
+          start: 0,
+          end: 100
+        },
+        {
+          start: 0,
+          end: 100
+        }
+      ],
+      series: [
+        {
+          name: 'Vitesse',
+          type: 'line',
+          smooth: true,
+          symbol: 'none',
+          areaStyle: {},
+          data: this.chartData
+        }
+      ]
+    };
+  }
+
+  latLngAllPath: any[] = [];
+  latLngCurrentPath: any[] = [];
+  latLngPathStartEnd: any[] = [];
+  showMap: boolean = true;
+  drawPath(index: number, show: boolean = true) {
+    var pathIconStart: object = {
+      labelOrigin: { x: 0, y: 0 }, //16,48
+      url: "./assets/images/pinStart.png",
+      scaledSize: {
+        width: 60,//20
+        height: 60//40
+      }
+    };
+    var pathIconEnd: object = {
+      labelOrigin: { x: 0, y: 0 }, //16,48
+      url: "./assets/images/pinEnd.png",
+      scaledSize: {
+        width: 60,//20
+        height: 60//40
+      }
+    };
+    this.showMap = !show;
+    this.latLngCurrentPath = [];
+    this.latLngPathStartEnd = [];
+    if (show) {
+      this.latLngCurrentPath = this.latLngAllPath[index];
+      let start = this.latLngCurrentPath[0];
+      start.icon = pathIconStart;
+      this.latLngPathStartEnd.push(start);
+      let end = this.latLngCurrentPath[this.latLngCurrentPath.length - 1]
+      end.icon = pathIconEnd;
+      this.latLngPathStartEnd.push(end);
+      this.mockDirections();
+    }
+  }
+
+  map: any;
+  onMapReady(map: any) {
+    this.map = map;
+  }
+  locationArray: any[];
+  mockDirections() {
+    this.locationArray = this.latLngCurrentPath.map(m => {
+      var pt = new google.maps.LatLng(m.GPSPoint_lat, m.GPSPoint_lon);
+      return pt;
+    });
+    this.initRoute();
+  }
+  travelMarker: TravelMarker = null;
+  initRoute() {
+    const options: TravelMarkerOptions = {
+      map: this.map,
+      speed: 50, // default:10 marker animation speed
+      interval: 10, // default:10 marker refresh time
+      speedMultiplier: 1,
+      markerOptions: {
+        title: 'Travel Marker',
+        animation: google.maps.Animation.DROP,
+        icon: {
+          url: './assets/images/traveller.png',
+          // This marker is 20 pixels wide by 32 pixels high.
+          animation: google.maps.Animation.DROP,
+          // size: new google.maps.Size(256, 256),
+          scaledSize: new google.maps.Size(128, 128),
+          // The origin for this image is (0, 0).
+          origin: new google.maps.Point(0, 0),
+          // The anchor for this image is the base of the flagpole at (0, 32).
+          anchor: new google.maps.Point(53, 110),
+        },
+      }
+    };
+    // define marker
+    this.travelMarker = new TravelMarker(options);
+
+    // add marker location
+    //this.travelMarker.addLocation(this.locationArray);
+
+    //setTimeout(() => this.play(), 2000);
+  }
+
+  // play animation
+  play() {
+    this.travelMarker.play();
+  }
+
+}
